d3a9011910fa8782f4012bdfe4cd4f1c3ef3f032
Branden Seaberry <brandenseaberry@gmail.com>
2024-09-12 18:38:26 -0500

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/counter/campsitesSlice';
+import { commentsReducer } from '../features/comments/commentSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
+import { promotionsReducer } from '../features/promotions/promotionSlice';
 
 export const store = configureStore({
   reducer: {
-    campsites: campsitesReducer
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer
   },
 });
--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,10 +1,11 @@
 import { Col } from 'reactstrap';
 import Partner from './Partner';
+import { useSelector } from 'react-redux';
 import { selectAllPartners } from './partnersSlice';
 import { PARTNERS } from '../../app/shared/PARTNERS';
 
 const PartnersList = () => {
-    const partners = selectAllPartners();
+    const partners =useSelector( selectAllPartners);
     return (
         <Col className='mt-4'>
             {PARTNERS.map((partner) => {
